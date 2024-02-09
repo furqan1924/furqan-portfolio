f6986efdf2dd8ba04f449a1dacc98d7e835b3238
Muhammad Furqan <furqan1924@gmail.com>
2024-02-09 22:42:17 +0500

    rename cv

--- src/components/images.jsx
+++ src/components/images.jsx
@@ -3,5 +3,5 @@ export  const IMAGES = {
      image2 : new URL('../images/Logo.png', import.meta.url).href,
     // background: new URL("../images/light.jpeg",import.meta.url).href,
      loding:new URL('../images/loding.gif', import.meta.url).href,
-      resume: new URL("../images/Forentend_React_Developer.pdf",import.meta.url).href,
+      resume: new URL("../images/Frontend_React_Developer.pdf",import.meta.url).href,
 }
